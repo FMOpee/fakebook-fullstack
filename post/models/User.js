d6b4e0b02175a9027693df304bda31ac474a14d6
Fm-Man <bsse1102@iit.du.ac.bd>
2022-09-11 10:37:58 +0600

    idk

---
+++ post/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+// const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    firstName: {type: String, reuired: true},
+    lastName: {type: String, reuired: true},
+    email: {type: String, reuired: true},
+    password: {type: String, reuired: true},
+    profilePicture:{type:String, default:""},
+    coverPicture:{type:String, default:""},
+    followers:{type:Array,default:[]},
+    following:{type:Array,default:[]},
+    isAdmin:{type:Boolean, default:false}
+},{timestamps:true}
+);
+
+// userSchema.methods.generateAuthToken = function () {
+//     const token = jwt.sign({fullName: this.firstName + " " + this.lastName}, process.env.JWT_PVT_KEY, {expiresIn: "7d"});
+//     return token;
+// }
+
+const User = mongoose.model("User", userSchema);
+
+
+module.exports = User;
